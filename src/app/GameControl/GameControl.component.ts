37d1d3b8f12e54b51b221031a8fa99e591a94051
Andreas Boll Nielsen <andreas@boll-nielsen.dk>
2021-12-06 13:27:52 +0100

    created Assignment 4 project

---
+++ src/app/GameControl/GameControl.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-GameControl',
+  templateUrl: './GameControl.component.html',
+  styleUrls: ['./GameControl.component.css']
+})
+export class GameControlComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
